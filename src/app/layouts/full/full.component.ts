51f0ffc4d226124e1ac060c7fb4d6aa09c49ef55
jasonx <caranzojason@gmail.com>
2020-09-17 22:44:31 +0800

    default color

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -32,9 +32,9 @@ export class FullComponent implements OnInit {
     sidebarpos: 'fixed', // two possible values: fixed, absolute
     headerpos: 'fixed', // two possible values: fixed, absolute
     boxed: 'full', // two possible values: full, boxed
-    navbarbg: 'skin6', // six possible values: skin(1/2/3/4/5/6)
-    sidebarbg: 'skin5', // six possible values: skin(1/2/3/4/5/6)
-    logobg: 'skin6' // six possible values: skin(1/2/3/4/5/6)
+    navbarbg: 'skin4', // six possible values: skin(1/2/3/4/5/6)
+    sidebarbg: 'skin4', // six possible values: skin(1/2/3/4/5/6)
+    logobg: 'skin4' // six possible values: skin(1/2/3/4/5/6)
   };
 
   Logo() {
